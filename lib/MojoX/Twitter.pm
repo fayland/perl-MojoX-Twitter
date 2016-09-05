4c4b3ac3a97681d67a4be7f0dad36828b1657fe1
Fayland Lam <fayland@gmail.com>
2016-09-05 13:25:58 +0800

    0.05

--- lib/MojoX/Twitter.pm
+++ lib/MojoX/Twitter.pm
@@ -11,7 +11,7 @@ use Digest::SHA 'hmac_sha1';
 use MIME::Base64 'encode_base64';
 use URI::Escape 'uri_escape_utf8';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 has 'ua' => sub {
     my $ua = Mojo::UserAgent->new;
